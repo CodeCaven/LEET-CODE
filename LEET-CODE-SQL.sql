0348701e56f819f09e6d04d01399db600fedbd6a
CodeCaven <simon.j.caven@gmail.com>
2025-10-31 19:17:51 +1100

    More SQL Problems

--- LEET-CODE-SQL.sql
+++ LEET-CODE-SQL.sql
@@ -20,3 +20,25 @@ group by s1.Id
 order by Score DESC;
 
 -- 584: Customer Referee
+select name
+from Customer
+where referee_id != 2 or referee_id is null;
+
+-- 182: Duplicate Emails
+SELECT email FROM Person
+GROUP BY email HAVING COUNT(email) > 1;
+
+--184: Department Highest Salary
+SELECT d.name as "Department", e.name as "Employee", e.salary as "Salary"
+FROM Employee e, Department d
+WHERE e.departmentId = d.id
+AND
+(e.DepartmentId, e.Salary) in
+(SELECT e.DepartmentId, max(e.Salary) FROM Employee e GROUP BY e.DepartmentId);
+
+-- 180: Consecutive Numbers
+-- Example uses Common Table Expression and Window Functions
+With cte_num AS (Select Num AS V1, LEAD(num,1) Over (order by id) AS V2, LEAD(num,2) Over (order by id) AS V3 From Logs)
+Select V1 AS ConsecutiveNums from cte_num
+where V1=V2
+And V2=V3;
